944fe761fceb95ac504ee5473f9d2b04a2ccbc1e
Akuya Ekorot <akuyaekorot@gmail.com>
2023-05-02 16:52:07 +0300

    page

--- src/components/blocks/Page.jsx
+++ src/components/blocks/Page.jsx
@@ -1,10 +1,10 @@
 import { Editable, Slate, withReact } from "slate-react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Editor, createEditor } from "slate";
 import { initialValue, onKeyDown, renderElement } from "@/utils/page/";
 import CommandBox from "../CommandBox";
 
-const Page = () => {
+const Page = (props) => {
   const [editor] = useState(() => withReact(createEditor()));
   const [value, setValue] = useState(initialValue);
 
@@ -12,29 +12,38 @@ const Page = () => {
   const [coords, setCoords] = useState({ x: 0, y: 200 });
   const [commandBox, setCommandBox] = useState(false);
 
+  useEffect(() => {
+    props.setPageHeight(props.pageRef.current.clientHeight);
+  }, [value])
+
   return (
-    <Slate editor={editor} value={value} onChange={setValue}>
-      <div className="items-center font-mono text-xs flex flex-col gap-4">
-        <div className="relative w-[594px]">
-          {commandBox && <CommandBox coords={coords} />}
+    <div {...props.attributes} contentEditable={false}>
+      <div className="hidden">{props.children}</div>
+      <Slate editor={editor} value={value} onChange={setValue}>
+        <div className="items-center font-mono text-xs flex flex-col gap-4 bg-slate-100">
+          <div className="relative w-[594px]">
+            {commandBox && <CommandBox coords={coords} />}
+          </div>
+          <div ref={props.pageRef}>
+            <Editable
+              className="w-[594px] pl-[108px] pr-[62px] pt-[72px] pb-[84px] border border-slate-300 bg-white"
+              renderElement={(props) => renderElement(props)}
+              onKeyDown={(event) =>
+                onKeyDown(
+                  event,
+                  editor,
+                  Editor,
+                  element,
+                  setElement,
+                  setCoords,
+                  setCommandBox
+                )
+              }
+            ></Editable>
+          </div>
         </div>
-        <Editable
-          className="w-[594px] pl-[108px] pr-[62px] pt-[72px] border-x border-slate-300"
-          renderElement={(props) => renderElement(props)}
-          onKeyDown={(event) =>
-            onKeyDown(
-              event,
-              editor,
-              Editor,
-              element,
-              setElement,
-              setCoords,
-              setCommandBox
-            )
-          }
-        />
-      </div>
-    </Slate>
+      </Slate>
+    </div>
   );
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,25 @@
 import { Page } from "@/components/blocks";
+import { useRef, useState } from "react";
 
 export default function Home() {
-  return <Navigation />;
+  const [pageHeight, setPageHeight] = useState(0);
+  const pageRef = useRef();
+
+  return (
+    <>
+      <Navigation />
+      <div>{pageHeight}</div>
+      <Page pageRef={pageRef} setPageHeight={setPageHeight} />
+    </>
+  );
 }
 
 const Navigation = () => {
   return (
-    <>
-      <nav className="w-full z-10 h-12 shadow-lg sticky top-0 bg-slate-100 shadow-lg font-mono flex justify-center items-center rounded">
-        <div>
-          <p className="text-xl font-bold">TUNGA</p>
-        </div>
-      </nav>
-     <Page />
-    </>
+    <nav className="w-full z-10 h-12 shadow-lg sticky top-0 bg-slate-100 shadow-lg font-mono flex justify-center items-center rounded">
+      <div>
+        <p className="text-xl font-bold">TUNGA</p>
+      </div>
+    </nav>
   );
 };
